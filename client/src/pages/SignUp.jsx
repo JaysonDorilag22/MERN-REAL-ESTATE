21bb34475caa46c8e45c6912e1cf57da92109a33
JaysonDorilag22 <jaysondorilag1@gmail.com>
2023-10-25 09:08:12 +0800

    create sign in api route

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
-// import OAuth from '../components/OAuth';
+import axios from 'axios';
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
+
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       setLoading(true);
-      const res = await fetch('/api/auth/signup', {
-        method: 'POST',
+      const response = await axios.post('/api/auth/signup', formData, {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData),
       });
-      const data = await res.json();
+
+      const data = response.data;
       console.log(data);
+
       if (data.success === false) {
         setLoading(false);
         setError(data.message);
@@ -39,9 +40,8 @@ export default function SignUp() {
       setLoading(false);
       setError(error.message);
     }
-
-    console.log(FormData);
   };
+
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
@@ -70,11 +70,10 @@ export default function SignUp() {
 
         <button
           disabled={loading}
-          className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+          className='bg-blue-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
           {loading ? 'Loading...' : 'Sign Up'}
         </button>
-        {/* <OAuth/> */}
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
@@ -86,3 +85,92 @@ export default function SignUp() {
     </div>
   );
 }
+
+// import { useState } from 'react';
+// import { Link, useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+// // import OAuth from '../components/OAuth';
+
+// export default function SignUp() {
+//   const [formData, setFormData] = useState({});
+//   const [error, setError] = useState(null);
+//   const [loading, setLoading] = useState(false);
+//   const navigate = useNavigate();
+//   const handleChange = (e) => {
+//     setFormData({
+//       ...formData,
+//       [e.target.id]: e.target.value,
+//     });
+//   };
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     try {
+//       setLoading(true);
+//       const res = await fetch('/api/auth/signup', {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(formData),
+//       });
+//       const data = await res.json();
+//       console.log(data);
+//       if (data.success === false) {
+//         setLoading(false);
+//         setError(data.message);
+//         return;
+//       }
+//       setLoading(false);
+//       setError(null);
+//       navigate('/sign-in');
+//     } catch (error) {
+//       setLoading(false);
+//       setError(error.message);
+//     }
+
+//     console.log(FormData);
+//   };
+//   return (
+//     <div className='p-3 max-w-lg mx-auto'>
+//       <h1 className='text-3xl text-center font-semibold my-7'>Sign Up</h1>
+//       <form onSubmit={handleSubmit} className='flex flex-col gap-4'>
+//         <input
+//           type='text'
+//           placeholder='username'
+//           className='border p-3 rounded-lg'
+//           id='username'
+//           onChange={handleChange}
+//         />
+//         <input
+//           type='email'
+//           placeholder='email'
+//           className='border p-3 rounded-lg'
+//           id='email'
+//           onChange={handleChange}
+//         />
+//         <input
+//           type='password'
+//           placeholder='password'
+//           className='border p-3 rounded-lg'
+//           id='password'
+//           onChange={handleChange}
+//         />
+
+//         <button
+//           disabled={loading}
+//           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
+//         >
+//           {loading ? 'Loading...' : 'Sign Up'}
+//         </button>
+//         {/* <OAuth/> */}
+//       </form>
+//       <div className='flex gap-2 mt-5'>
+//         <p>Have an account?</p>
+//         <Link to={'/sign-in'}>
+//           <span className='text-blue-700'>Sign in</span>
+//         </Link>
+//       </div>
+//       {error && <p className='text-red-500 mt-5'>{error}</p>}
+//     </div>
+//   );
+// }
